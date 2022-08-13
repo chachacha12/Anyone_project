3719440de522a03236f42875d1c0de67f67d2112
chachacha12 <chaminwoo369@gmail.com>
2022-08-14 00:30:27 +0900

    Complete onCampus - Tips

--- lib/home/oncampus_content/Tips_hero_second.dart
+++ lib/home/oncampus_content/Tips_hero_second.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Tips_hero_second extends StatefulWidget {
    Tips_hero_second(this.tips_document, {Key? key}) : super(key: key);
@@ -18,18 +19,23 @@ class _Tips_hero_secondState extends State<Tips_hero_second> {
   @override
   Widget build(BuildContext context) {
 
+    //Tips.dart에서 가져온 이미지 리스트들을 imgList에 저장. - 타입을 리스트타입으로 바꿔주기 위해
     for(var img in widget.tips_document['imagepath']){
       imgList.add(img);
     }
 
-    //캐러셀슬라이더에 넣어줄 items옵션값을 변수로 만들어줌 - 슬라이드해서 보여줄 이미지들과 텍스트값
-    final List<Widget> imageSliders = imgList
+    //팁 부가설명해주는 텍스트 - 줄바꿈이 파베에선 되지않아서 여기서 줄바꿈을 해준후 보여주기위함.
+    var text = widget.tips_document['text'].toString().replaceAll("\\n", "\n");
+
+
+    //캐러셀슬라이더에 넣어줄 items옵션값임.  - 슬라이드해서 보여줄 이미지들과 텍스트값들임
+    final List<Widget> imageSliders = imgList   //이미지리스트를 넣어줌
         .map((item) => Container(        //item하나하나가 string으로된 사진주소 문자열임.
       child: Container(
         margin: EdgeInsets.all(5.0),
         child: ClipRRect(
             borderRadius: BorderRadius.all(Radius.circular(5.0)),
-            child: Stack(
+            child: Stack(       //위젯들을 겹치게 배치가능. 여기선 이미지위젯과 텍스트위젯을 겹치게 배치
               children: <Widget>[
                 Image.network(item, fit: BoxFit.cover, width: 1000.0),
                 Positioned(
@@ -39,7 +45,8 @@ class _Tips_hero_secondState extends State<Tips_hero_second> {
                   child: Container(
                     decoration: BoxDecoration(
 
-                      gradient: LinearGradient(
+                      /*
+                      gradient: LinearGradient(  //Stack위젯에서 밑의 텍스트위젯에 그라데이션 색깔 넣어주는 옵션
                         colors: [
                           Color.fromARGB(200, 0, 0, 0),
                           Color.fromARGB(0, 0, 0, 0)
@@ -47,14 +54,14 @@ class _Tips_hero_secondState extends State<Tips_hero_second> {
                         begin: Alignment.bottomCenter,
                         end: Alignment.topCenter,
                       ),
-
+                       */
                     ),
                     padding: EdgeInsets.symmetric(
                         vertical: 10.0, horizontal: 20.0),
-                    child: Text(
+                    child: Text(                  //슬라이드이미지들 밑에 뜰 텍스트
                       '',  //No. ${imgList.indexOf(item)} image
                       style: TextStyle(
-                        color: Colors.white,
+                        color: Colors.black45,
                         fontSize: 10.0,
                         fontWeight: FontWeight.normal,
                       ),
@@ -68,20 +75,23 @@ class _Tips_hero_secondState extends State<Tips_hero_second> {
         .toList();
 
 
-
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           SliverAppBar(title: Text(
-            'tips',
+            widget.tips_document['title'],
           ),
+            //backgroundColor: Colors.transparent,
+            centerTitle: true,
+              floating: true  //밑으로 스크롤시 앱바 사라짐
           ),
 
           SliverToBoxAdapter(
             child: Container(
-                  child: Hero(
+                  margin: EdgeInsets.fromLTRB(0, 20.h, 0, 0),
+                  child: Hero(        //Hero위젯
                     tag: widget.tips_document['title'],
-                    child: CarouselSlider(
+                    child: CarouselSlider(         //이미지슬라이드 해주는 위젯
                       options: CarouselOptions(
                         //autoPlay: true,
                         aspectRatio: 2.0,
@@ -93,6 +103,30 @@ class _Tips_hero_secondState extends State<Tips_hero_second> {
                 ),
             ),
 
+          SliverToBoxAdapter(
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.fromLTRB(20.w, 10.h, 20.w, 0),
+              child: Text(widget.tips_document['tag'], style: TextStyle(
+                fontSize: 20.sp,
+                fontWeight: FontWeight.bold,
+              ),),
+            ),
+          ),
+
+          SliverToBoxAdapter(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                color: Colors.tealAccent
+              ),
+              margin: EdgeInsets.fromLTRB(20.w, 30.h, 20.w, 50.h),
+              padding: EdgeInsets.all(15.w),
+              child: Text(text, style: TextStyle(
+                fontSize: 15.sp
+              ),),
+            ),
+          )
 
 
         ],
