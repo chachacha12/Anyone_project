10954890cbdc434cb225319c7c9cbe2868c2bf52
chachacha12 <chaminwoo369@gmail.com>
2022-08-26 14:40:01 +0900

    Modify my.dart UI

--- lib/my/my.dart
+++ lib/my/my.dart
@@ -15,13 +15,18 @@ class My extends StatefulWidget {
 
 class _MyState extends State<My> {
 
+  var name;  //사용자가 로그인했을때 사용한 이름을 보여줌
+
   /* DatePicker 띄우기 */
   showDatePickerPop() {
     Future<DateTime?> selectedDate = showDatePicker(
       context: context,
-      initialDate: DateTime.now(), //초기값
-      firstDate: DateTime(2022), //시작일
-      lastDate: DateTime(2030), //마지막일
+      initialDate: DateTime.now(),
+      //초기값
+      firstDate: DateTime(2022),
+      //시작일
+      lastDate: DateTime(2030),
+      //마지막일
       builder: (BuildContext context, Widget? child) {
         return Theme(
           data: ThemeData.light(), //다크 테마
@@ -30,24 +35,27 @@ class _MyState extends State<My> {
       },
     );
 
-    selectedDate.then((dateTime) {      //날짜 선택 직후에 할일
+    selectedDate.then((dateTime) { //날짜 선택 직후에 할일
       //유저가 cancel안하고 특정날짜를 선택했을때
-      if(dateTime != null){
+      if (dateTime != null) {
         var formatter = DateFormat('yyyy-MM-dd');
-        var formatDate = formatter.format(dateTime);  //스트링타입으로 만들어진 유저가 선택한 날짜값
-        var date = formatter.parse(formatDate);  //유저가 선택한 날짜값을 Date타입으로. 두 날짜 빼기 해야해서
+        var formatDate = formatter.format(dateTime); //스트링타입으로 만들어진 유저가 선택한 날짜값
+        var date = formatter.parse(
+            formatDate); //유저가 선택한 날짜값을 Date타입으로. 두 날짜 빼기 해야해서
         //날짜 State를 선택한 String 날짜값으로 변경해줌
         context.read<Store1>().ChangeDate(formatDate.toString());
 
         //특정 날짜와 날짜 사이의 디데이를 계산
-        var difference = date.difference(getToday() ).inDays;
-        var result ='';
-        if(difference > 0){
-          result = '- '+difference.toString();
-        }else if(difference < 0){
-          difference = -difference;    //부호를 +로 변경시켜주기위함
-          result = '+ '+difference.toString();
-        }else{      //디데이일때
+        var difference = date
+            .difference(getToday())
+            .inDays;
+        var result = '';
+        if (difference > 0) {
+          result = '- ' + difference.toString();
+        } else if (difference < 0) {
+          difference = -difference; //부호를 +로 변경시켜주기위함
+          result = '+ ' + difference.toString();
+        } else { //디데이일때
           result = '- day';
         }
         //디데이 계산결과값 스트링을 state에 넘겨줌
@@ -74,7 +82,6 @@ class _MyState extends State<My> {
     return dateTime;
   }
 
-
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
@@ -98,74 +105,51 @@ class _MyState extends State<My> {
           floating: true,
         ),
 
-        //이메일, 풀네임, 닉네임을 보여줌
-        SliverToBoxAdapter(
-          child: Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-            height: 150.0.h,
-            child: Container(
-              color: Colors.green,
-              width: 200.w,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text('E-mail:  '),
-                      Text('12zxccha@'),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text('Full Name:  '),
-                      Text('chaminwoo')
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text('Nickname:  '),
-                      Text('chachacha')
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
 
+        //이름 보여줌
         SliverToBoxAdapter(
-          child: Container(
-            alignment: Alignment.center,
-            height: 300.h,
-            width: 100.0.w,
-            child: Container(
-              alignment: Alignment.center,
-              color: Colors.yellow,
-              height: 300.0.h,
-              width: 250.w,
+            child: Card(
+              margin: EdgeInsets.fromLTRB(20.w, 50.h, 20.w, 0.h),
+              elevation: 5,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Text('D '+context.watch<Store1>().dday),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20.w, 20.w, 20.w, 60.w),
+                    child: Text(
+                        'The time left before ${name} leaves for home country !',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(color: Colors.black, fontSize: 20.sp)),
+                  ),
+
+                  SizedBox(
+                    height: 50.h,
+                    child: Text('D ' + context
+                        .watch<Store1>()
+                        .dday, style: TextStyle(fontSize: 30.sp,
+                    fontWeight: FontWeight.bold),),
+                  ),
+
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('Departure Date:     '),
-                      TextButton(onPressed: (){
-                        showDatePickerPop();  //날짜픽업위젯보여줌
-                      }, child: Text(context.watch<Store1>().date)
+                      Text('Departure Date: ', style: TextStyle(
+                        fontSize: 20.sp
+                      )),
+                      TextButton(onPressed: () {
+                        showDatePickerPop(); //날짜픽업위젯보여줌
+                      }, child: Text(context
+                          .watch<Store1>()
+                          .date,
+                          style: TextStyle(
+                              fontSize: 20.sp
+                          ))
                       )
                     ],
                   ),
-
                 ],
               ),
-              ),
-          ),
+            )
         ),
 
       ],
