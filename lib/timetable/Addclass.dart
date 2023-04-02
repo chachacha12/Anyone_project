5f2994406af25a4959a2c62fbec7d3a491e3c840
chachacha12 <chaminwoo369@gmail.com>
2023-04-03 02:44:00 +0900

    modify Addclass widget design

--- lib/timetable/Addclass.dart
+++ lib/timetable/Addclass.dart
@@ -16,7 +16,7 @@ class Addclass extends StatefulWidget {
 class _AddclassState extends State<Addclass> {
 
   ///텍스트필드 값을 저장할 리스트. 순서대로:   수업제목+수업위치, 요일값(숫자로), starthour, startminute, endhour,endminute
-  List<dynamic> inputData =['', 0,   0, 0, 0, 0 ];
+  List<dynamic> inputData = ['', 0, 0, 0, 0, 0];
 
   ///타임피커 통해 start time과 end time 저장
   var starttime_btn = 'Start time';
@@ -24,7 +24,7 @@ class _AddclassState extends State<Addclass> {
 
 
   ///요일선택 드롭박스를 위함
-  final daylist = ['Monday', 'Tuesday','Wednesday','Thursday','Friday'];
+  final daylist = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
   String selectedday = '';
 
   ///빌딩선택 드롭박스를 위함
@@ -64,16 +64,18 @@ class _AddclassState extends State<Addclass> {
     );
     selectedTime.then((timeOfDay) {
       setState(() {
-        if(option == 0){   //0이면 start time 선택했을때고 1이면 endtime 선택했을때임
-          starttime_btn = '${timeOfDay?.hour}:${timeOfDay?.minute }';
-        }else{
-          endtime_btn = '${timeOfDay?.hour}:${timeOfDay?.minute }';
-        }
+        if (timeOfDay?.hour != null) {
+          if (option == 0) { //0이면 start time 선택했을때고 1이면 endtime 선택했을때임
+            starttime_btn = '${timeOfDay?.hour}:${timeOfDay?.minute }';
+          } else {
+            endtime_btn = '${timeOfDay?.hour}:${timeOfDay?.minute }';
+          }
+        } else {
 
+        }
       });
     });
   }
-
 
 
   @override
@@ -98,120 +100,147 @@ class _AddclassState extends State<Addclass> {
         actions: [
           Container(
             margin: EdgeInsets.fromLTRB(0.w, 15.h, 23.w, 15.h),
-            width:  60.w,
-            child: ElevatedButton(onPressed: (){  /// 일정추가 완료 버튼 클릭시
+            width: 60.w,
+            child: ElevatedButton(onPressed: () {
+              /// 일정추가 완료 버튼 클릭시
 
             }, child: Text('Done'),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green,
-                textStyle: TextStyle(
-                  fontWeight: FontWeight.w500,
-                fontSize: 12.sp)
-            )
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    textStyle: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 12.sp)
+                )
             ),
           )
         ],
       ),
 
       body: Container(
-        color: Colors.white,
-        padding: EdgeInsets.fromLTRB(20.w, 20.h, 20.w, 20.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            TextField(
-                onChanged: (text){ inputData[0] = text;  },
-                decoration: getTextFieldStyle('Class Name'),
-            ),
-
-            Container(
-              alignment: Alignment.centerLeft,
-              margin: EdgeInsets.fromLTRB(3.w, 15.h, 0.w, 5.h),
-              child: Text('Class Time Info', style: TextStyle(
-                color: Colors.green
-              ),),
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                ///드롭다운박스를 통해 요일 선택
-                Flexible(child: DropdownButton(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  isExpanded: true, //글씨길면 줄바꿈해줌
-                  value: selectedday,
-                  items: daylist
-                      .map((e) => DropdownMenuItem(
-                    value: e, // 선택 시 onChanged 를 통해 반환할 value
-                    child: Text(e),
-                  ))
-                      .toList(),
-                  onChanged: (value) { // items 의 DropdownMenuItem 의 value 반환
-                    setState(() {
-                      selectedday = value.toString();
-                    });
+          color: Colors.white,
+          padding: EdgeInsets.fromLTRB(20.w, 20.h, 20.w, 20.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 45.h,
+                child: TextField(
+                  onChanged: (text) {
+                    inputData[0] = text;
                   },
+                  decoration: getTextFieldStyle('Class Name'),
                 ),
-                flex: 4),
-                SizedBox(width: 10.w,),
-                Flexible(child: ElevatedButton(
-                child: Text(starttime_btn),
-                   onPressed:(){
-                    showTimePickerPop(0);
-                  },
-                ), flex:3),
+              ),
 
-                SizedBox(width: 10.w,),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.fromLTRB(3.w, 15.h, 0.w, 5.h),
+                child: Text('Class Time Info', style: TextStyle(
+                    color: Colors.green
+                ),),
+              ),
 
-                Flexible(child: TextField(
-                  onChanged: (text){ inputData[3] = text;  },
-                  decoration: getTextFieldStyle('End Time'),
-                ), flex: 3),
-              ],
-            ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ///드롭다운박스를 통해 요일 선택
+                  Flexible(child: DropdownButton(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    isExpanded: true,
+                    //글씨길면 줄바꿈해줌
+                    value: selectedday,
+                    items: daylist
+                        .map((e) =>
+                        DropdownMenuItem(
+                          value: e, // 선택 시 onChanged 를 통해 반환할 value
+                          child: Text(e),
+                        ))
+                        .toList(),
+                    onChanged: (value) { // items 의 DropdownMenuItem 의 value 반환
+                      setState(() {
+                        selectedday = value.toString();
+                      });
+                    },
+                  ),
+                      flex: 3),
+                  SizedBox(width: 10.w,),
+                  ///start time값 받는 버튼
+                  Flexible(child: OutlinedButton(
+                    style: getButtonStyle(),
+                    child: Text(starttime_btn, style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15.sp,
+                    ),),
+                    onPressed: () {
+                      showTimePickerPop(0);
+                    },
+                  ), flex: 3),
 
-            Container(
-              alignment: Alignment.centerLeft,
-              margin: EdgeInsets.fromLTRB(3.w, 15.h, 0.w, 5.h),
-              child: Text('Class Location Info', style: TextStyle(
-                  color: Colors.green
-              ),),
-            ),
+                  SizedBox(width: 10.w,),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                ///드롭다운박스를 통해 빌딩선택
-                Flexible(child: DropdownButton(
-                  alignment:Alignment.center,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  itemHeight: 65.h,
-                  menuMaxHeight: 500.h,
-                  isExpanded: true, //글씨길면 줄바꿈해줌
-                  value: selectedbuilding,
-                  items: buildinglist
-                      .map((e) => DropdownMenuItem(
-                    value: e, // 선택 시 onChanged 를 통해 반환할 value
-                    child: Text(e,),
-                  ))
-                      .toList(),
-                  onChanged: (value) { // items 의 DropdownMenuItem 의 value 반환
-                    setState(() {
-                      selectedbuilding = value.toString();
-                    });
-                  },
-                ), flex: 5),
+                  ///End time값 받는 버튼
+                  Flexible(child: OutlinedButton(
+                    style: getButtonStyle(),
+                    child: Text(endtime_btn, style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15.sp,
+                    ),),
+                    onPressed: () {
+                      showTimePickerPop(1);
+                    },
+                  ), flex: 3),
+                ],
+              ),
 
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.fromLTRB(3.w, 15.h, 0.w, 5.h),
+                child: Text('Class Location Info', style: TextStyle(
+                    color: Colors.green
+                ),),
+              ),
 
-                SizedBox(width: 10.w,),
-                Flexible(child: TextField(
-                  onChanged: (text){ inputData[5] = text;  },
-                  decoration: getTextFieldStyle('Room number'),
-                ), flex: 5),
-              ],
-            ),
-          ],
-        )
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ///드롭다운박스를 통해 빌딩선택
+                  Flexible(child: DropdownButton(
+                    alignment: Alignment.center,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    itemHeight: 65.h,
+                    menuMaxHeight: 500.h,
+                    isExpanded: true,
+                    //글씨길면 줄바꿈해줌
+                    value: selectedbuilding,
+                    items: buildinglist
+                        .map((e) =>
+                        DropdownMenuItem(
+                          value: e, // 선택 시 onChanged 를 통해 반환할 value
+                          child: Text(e,),
+                        ))
+                        .toList(),
+                    onChanged: (value) { // items 의 DropdownMenuItem 의 value 반환
+                      setState(() {
+                        selectedbuilding = value.toString();
+                      });
+                    },
+                  ), flex: 5),
+
+                  SizedBox(width: 10.w,),
+                  Flexible(child: Container(
+                    padding: EdgeInsets.fromLTRB(0, 5.h, 0, 0),
+                    height: 40.h,
+                    child: TextField(
+                      onChanged: (text) {
+                        inputData[5] = text;
+                      },
+                      decoration: getTextFieldStyle('Room number'),
+                    ),
+                  ), flex: 5),
+                ],
+              ),
+            ],
+          )
       ),
     );
   }
@@ -223,14 +252,14 @@ getTextFieldStyle(hint) {
     labelText: hint,
     labelStyle: TextStyle(fontSize: 13.sp, color: Colors.black),
     enabledBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(10.w),
+      borderRadius: BorderRadius.circular(4.w),
       borderSide: BorderSide(
         color: Colors.black,
         width: 1.0,
       ),
     ),
     focusedBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(10.w),
+      borderRadius: BorderRadius.circular(4.w),
       borderSide: BorderSide(
         color: Colors.black,
         width: 1.0,
@@ -239,8 +268,17 @@ getTextFieldStyle(hint) {
   );
 }
 
-
-
+///start time과 end time 버튼의 스타일값 반환함수
+getButtonStyle(){
+  return OutlinedButton.styleFrom(
+      primary: Colors.black, // 글자, 애니메이션 색상
+      backgroundColor: Colors.white, // 배경 색상
+      side: BorderSide(
+          color: Colors.black,
+          width: 1
+      )
+  );
+}
 
 
 
