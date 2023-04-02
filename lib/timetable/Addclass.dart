8c8367148faf29ccca238e7bc1c7e64d30981d93
chachacha12 <chaminwoo369@gmail.com>
2023-04-02 22:18:05 +0900

    modify Addclass widget

--- lib/timetable/Addclass.dart
+++ lib/timetable/Addclass.dart
@@ -1,6 +1,7 @@
-import 'package:anyone/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 
 class Addclass extends StatefulWidget {
@@ -11,10 +12,80 @@ class Addclass extends StatefulWidget {
 }
 
 
+
 class _AddclassState extends State<Addclass> {
 
-  ///텍스트필드 값을 저장할 리스트
-  List<dynamic> inputData =['','','','','',''];
+  ///텍스트필드 값을 저장할 리스트. 순서대로:   수업제목+수업위치, 요일값(숫자로), starthour, startminute, endhour,endminute
+  List<dynamic> inputData =['', 0,   0, 0, 0, 0 ];
+
+  ///타임피커 통해 start time과 end time 저장
+  var starttime_btn = 'Start time';
+  var endtime_btn = 'End time';
+
+
+  ///요일선택 드롭박스를 위함
+  final daylist = ['Monday', 'Tuesday','Wednesday','Thursday','Friday'];
+  String selectedday = '';
+
+  ///빌딩선택 드롭박스를 위함
+  final buildinglist = [
+    'Administration Bldg',
+    'Business Administration Bldg',
+    'Sanghuh Hall',
+    'Education Science Bldg',
+    'Art & Design Bldg',
+    'Konkuk Language Institute',
+    'Law School Bldg',
+    'Biomedical Science Bldg',
+    'Life Science Bldg',
+    'Animal Science Bldg',
+    'Admissions & Information Technology Bldg',
+    'Industry-University Cooperation Bldg',
+    'Veterinary Medicine Bldg',
+    'New Millennium Hall',
+    'Architecture Bldg',
+    'Hae-Bong Real Estate Bldg',
+    'Liberal Arts Bldg',
+    'Student Union Bldg',
+    'Engineering Bldg',
+    'New Engineering Bldg',
+    'Science Bldg',
+    'Chang-ui Bldg',
+    'Gymnasium',
+  ];
+  String selectedbuilding = '';
+
+
+  /// TimePicker 띄워주는 함수
+  void showTimePickerPop(int option) {
+    Future<TimeOfDay?> selectedTime = showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    selectedTime.then((timeOfDay) {
+      setState(() {
+        if(option == 0){   //0이면 start time 선택했을때고 1이면 endtime 선택했을때임
+          starttime_btn = '${timeOfDay?.hour}:${timeOfDay?.minute }';
+        }else{
+          endtime_btn = '${timeOfDay?.hour}:${timeOfDay?.minute }';
+        }
+
+      });
+    });
+  }
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      ///드롭다운박스 초기 선택된 아이템값
+      selectedday = daylist[0];
+      selectedbuilding = buildinglist[0];
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -64,16 +135,34 @@ class _AddclassState extends State<Addclass> {
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Flexible(child: TextField(
-                  onChanged: (text){ inputData[1] = text;  },
-                  decoration: getTextFieldStyle('Monday'),
-                ), flex: 4),
+                ///드롭다운박스를 통해 요일 선택
+                Flexible(child: DropdownButton(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  isExpanded: true, //글씨길면 줄바꿈해줌
+                  value: selectedday,
+                  items: daylist
+                      .map((e) => DropdownMenuItem(
+                    value: e, // 선택 시 onChanged 를 통해 반환할 value
+                    child: Text(e),
+                  ))
+                      .toList(),
+                  onChanged: (value) { // items 의 DropdownMenuItem 의 value 반환
+                    setState(() {
+                      selectedday = value.toString();
+                    });
+                  },
+                ),
+                flex: 4),
                 SizedBox(width: 10.w,),
-                Flexible(child: TextField(
-                  onChanged: (text){ inputData[2] = text;  },
-                  decoration: getTextFieldStyle('Start Time'),
-                ), flex: 3),
+                Flexible(child: ElevatedButton(
+                child: Text(starttime_btn),
+                   onPressed:(){
+                    showTimePickerPop(0);
+                  },
+                ), flex:3),
+
                 SizedBox(width: 10.w,),
+
                 Flexible(child: TextField(
                   onChanged: (text){ inputData[3] = text;  },
                   decoration: getTextFieldStyle('End Time'),
@@ -92,10 +181,28 @@ class _AddclassState extends State<Addclass> {
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Flexible(child: TextField(
-                  onChanged: (text){ inputData[4] = text;  },
-                  decoration: getTextFieldStyle('Building'),
+                ///드롭다운박스를 통해 빌딩선택
+                Flexible(child: DropdownButton(
+                  alignment:Alignment.center,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  itemHeight: 65.h,
+                  menuMaxHeight: 500.h,
+                  isExpanded: true, //글씨길면 줄바꿈해줌
+                  value: selectedbuilding,
+                  items: buildinglist
+                      .map((e) => DropdownMenuItem(
+                    value: e, // 선택 시 onChanged 를 통해 반환할 value
+                    child: Text(e,),
+                  ))
+                      .toList(),
+                  onChanged: (value) { // items 의 DropdownMenuItem 의 value 반환
+                    setState(() {
+                      selectedbuilding = value.toString();
+                    });
+                  },
                 ), flex: 5),
+
+
                 SizedBox(width: 10.w,),
                 Flexible(child: TextField(
                   onChanged: (text){ inputData[5] = text;  },
@@ -103,7 +210,6 @@ class _AddclassState extends State<Addclass> {
                 ), flex: 5),
               ],
             ),
-
           ],
         )
       ),
@@ -111,9 +217,8 @@ class _AddclassState extends State<Addclass> {
   }
 }
 
-
 ///텍스트필드 공통된 스타일값 반환해주는 함수
-getTextFieldStyle(hint){
+getTextFieldStyle(hint) {
   return InputDecoration(
     labelText: hint,
     labelStyle: TextStyle(fontSize: 13.sp, color: Colors.black),
@@ -133,6 +238,11 @@ getTextFieldStyle(hint){
     ),
   );
 }
+
+
+
+
+
 
 
 
--- lib/timetable/Timetable.dart
+++ lib/timetable/Timetable.dart
@@ -38,6 +38,15 @@ class _TimetableState extends State<Timetable> {
             allowAppointmentResize: true,
             allowDragAndDrop: true,
             viewNavigationMode: ViewNavigationMode.none,
+
+            /*
+            //뷰를 스와이프해서 바꿀때 감지해서 이벤트처리해줌
+            onViewChanged: (viewChangedDetails) {
+              List<DateTime> dates = viewChangedDetails.visibleDates;
+              print('dates: '+ dates.toString());
+            },
+             */
+
             initialDisplayDate: DateTime.now(),
             showCurrentTimeIndicator: true,
             view: CalendarView.workWeek,
@@ -59,13 +68,15 @@ class _TimetableState extends State<Timetable> {
     /// 특정날짜가 속한 주의 월요일 날짜 구하는 방법: today.day - (today.weekday - 1)
     /// 즉, 특정요일에 일정을 표시할거면 위에서 구한 월요일날짜에 +1, +2 등을 해서 이번주의 화요일, 수요일 등을 구할 수 있음
     final DateTime startTime =
-    DateTime(today.year, today.month, today.day - (today.weekday - 1)+4, 11,0);
+    DateTime(today.year, today.month, today.day - (today.weekday - 1)+4, 10,0);
+    print('startTime: '+startTime.toString());
 
     final DateTime endTime =
     DateTime(today.year, today.month, today.day - (today.weekday - 1)+4, 13,0);
+    print('endTime: '+endTime.toString());
 
     meetings.add(
-        Meeting('Conference', startTime, endTime, const Color(0xFF0F8644)));
+        Meeting('Conference', startTime, endTime, const Color(0xFF0F8644), false));
     return meetings;
   }
 }
@@ -104,12 +115,13 @@ class MeetingDataSource extends CalendarDataSource {
 
 ///일정객체 하나를 만드는 클래스
 class Meeting {
-  Meeting(this.eventName, this.from, this.to, this.background);
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
 
   String eventName;
   DateTime from;
   DateTime to;
   Color background;
+  bool isAllDay;
 }
 
 
