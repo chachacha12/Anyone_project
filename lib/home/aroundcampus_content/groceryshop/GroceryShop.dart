105f1281331313b5e2ddbf7e197e222c0514a986
chachacha12 <chaminwoo369@gmail.com>
2023-05-26 02:01:13 +0900

    modify GroceryShop widget UI

--- lib/home/aroundcampus_content/groceryshop/GroceryShop.dart
+++ lib/home/aroundcampus_content/groceryshop/GroceryShop.dart
@@ -1,10 +1,12 @@
-import 'package:anyone/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:shimmer/shimmer.dart';
+import 'package:provider/provider.dart';
 import '../../../NaverMapDeepLink.dart';
+import '../../../Provider/Provider.dart';
+import '../../../authentic/signup.dart';
 import '../../../various_widget.dart';
+import '../CommonWidget.dart';
 import 'GroceryShop_hero_second.dart';
 
 //파베 파이어스토어 사용을 위한 객체
@@ -19,20 +21,10 @@ class GroceryShop extends StatefulWidget {
 }
 
 class _GroceryShopState extends State<GroceryShop> with AutomaticKeepAliveClientMixin {
-
   var grocery_collection; //파이어스토어로부터 받아올 문서들 리스트를 여기에 넣어줄거임
   var count = 0;
-
-  //hero위젯을 통해 전환될 페이지로 보내줄 마트 컨텐츠 문서 하나임. 타입을 dynamic으로해야 어떤 타입이든 받을 수 있어서 이렇게함
-  dynamic grocery_document;
-
-  getData() async {
-    var result = await firestore.collection('grocery').get();
-    setState(() {
-      grocery_collection = result.docs; //컬랙션안의 문서리스트를 저장
-      count = result.size; //컬랙션안의 문서갯수를 가져옴
-    });
-  }
+  var imgList = []; //이미지들 주소 string값을 저장해줄 리스트
+  var isMyList = []; //어떤 index의 컨텐츠가 찜한 컨텐츠인지 확인해서 색상아이콘 넣어주기 위함
 
   @override
   void initState() {
@@ -40,97 +32,228 @@ class _GroceryShopState extends State<GroceryShop> with AutomaticKeepAliveClient
     getData();
   }
 
+  getData() async {
+    //컨텐츠 보여주기 위해 가져오는 데이터들
+    var result = await firestore.collection('grocery').get();
+
+    setState(() {
+      grocery_collection = result.docs; //컬랙션안의 문서리스트를 저장
+      count = result.size; //컬랙션안의 문서갯수를 가져옴
+    });
+    makeMyList();
+  }
+
+
+  ///isMyList 리스트에 값을 채워주는 함수.  어떤 index의 컨텐츠가 찜한 컨텐츠인지 확인해서 찜한컨텐츠만 색아이콘 보여주고, 클릭시 실시간으로 아이콘 색 변경해주기 위한작업
+  makeMyList(){
+    if(grocery_collection!=null){
+      isMyList.clear();
+      // 전체 컨텐츠 문서 하나씩 확인
+      print('@@@enter에서 모든 내찜문서들 도는 작업 진행');
+      for (var doc in grocery_collection) {
+        var exist = false;
+        //내 찜목록 문서들 하나씩 확인
+        for (var myDoc in context
+            .read<MyListStore>()
+            .fashionMyList) {
+          ///찜목록에 이미 존재
+          if (doc['name'] == myDoc['name']) {
+            exist = true;
+            isMyList.add(true);
+            break;
+          }
+        }
+        ///찜목록에 없음
+        if (!exist) {
+          isMyList.add(false);
+        }
+      }
+    }
+  }
+
+
+  ///파베의 내찜리스트에서 문서 삭제해주는 함수
+  deleteDoc(index) async {
+    try {
+      await firestore.collection(
+          'MyList')
+          .doc(auth.currentUser?.uid)
+          .collection('groceries')
+          .doc(
+          grocery_collection[index]['name'])
+          .delete();
+      print('삭제성공');
+
+      ///store에 찜목록 삭제로직
+      context.read<MyListStore>()
+          .deleteGroceries(
+          grocery_collection[index]);
+
+    } catch (e) {
+      print('에러');
+    }
+  }
+  ///파베의 내찜리스트에 문서 추가해주는 함수
+  addDoc(index) async {
+    try {
+      await firestore.collection(
+          'MyList')
+          .doc(auth.currentUser?.uid)
+          .collection('groceries')
+          .doc(
+          grocery_collection[index]['name'])
+          .set({
+        'name': grocery_collection[index]['name'],
+        'time': grocery_collection[index]['time'],
+        'imagepath': grocery_collection[index]['imagepath'],
+        'text': grocery_collection[index]['text'],
+        'address': grocery_collection[index]['address'],
+        'holiday': grocery_collection[index]['holiday'],
+      });
+      print('저장 성공');
+
+      ///store에 새로운 찜목록 추가로직
+      context.read<MyListStore>()
+          .addGroceries(
+          grocery_collection[index]);
+    } catch (e) {
+      print('에러');
+    }
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold( //fragment같은게 아닌 아예 새페이지를 띄울땐 Scaffold를 감싸서 띄워주어야 페이지 제대로 띄워지는듯
       body: CustomScrollView(
         slivers: [
 
-          /*
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: 240.0.h,
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              title:  Text('Choose a store\nregarding your needs',
-                textAlign: TextAlign.center, style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w500,
-                ),),
-
-              background: Image.asset(
-                'assets/GroceryShop/groceryshopping_background.jpg',
-                fit: BoxFit.cover,),
-            ),
-          ),
-           */
 
           SliverList(
             delegate: SliverChildBuilderDelegate(
                     (context, index) =>
-                    GestureDetector(
-                      child: Card( //리스트 속 각각의 객체 하나하나임
-                          elevation: 1,
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10.h, horizontal: 20.w),
 
-                          child: Container(
-                            margin: EdgeInsets.fromLTRB(0.w, 5.h, 0.w, 5.h),
-                            child: Row(
+                        Container( //컨텐츠 하나하나
+                            margin: EdgeInsets.symmetric(vertical: 20.h, horizontal: 10.w),
+                            child: Column(
                               mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                SizedBox(
-                                  width: 180.w,
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly, //스토어명, 영업시간, 주소, 휴무일정보
-                                    children: [
-                                      Text(' ' +
-                                          grocery_collection[index]['name'] +
-                                          '\n',
-                                          style: TextStyle(
-                                            fontSize: 14.sp,
-                                            fontWeight: FontWeight.bold,
-                                          )),
-                                      //네이버맵 url scheme값을 이용해서 딥링크 연결하는 동작을 위한 커스텀위젯
-                                      //인자값으로 각 컨텐츠의 풀네임값을 보내줌
-                                      NaverMapDeepLink( titlename : grocery_collection[index]['name'] ),
 
-                                      richtext(
-                                          Icon(Icons.access_time, size: 13.h),
-                                          grocery_collection[index]['time']),
-                                      richtext(Icon(
-                                          Icons.location_on_outlined, size: 13.h),
-                                          grocery_collection[index]['address']),
-                                      richtext(Icon(Icons.block, size: 13.h),
-                                          grocery_collection[index]['holiday']),
+                                Row( ///가게명과 찜버튼
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    ///가게명
+                                    Flexible(
+                                        fit: FlexFit.tight,
+                                        flex: 7,
+                                        child: contentsName( grocery_collection[index]['name']) ///CommonWidget파일안에 있는 함수
+                                    ),
+
+                                    ///찜버튼
+                                    Flexible(
+                                      //fit: FlexFit.loose,
+                                      flex: 1,
+                                      child: GestureDetector(
+                                          child: myListButton(isMyList[index]), ///CommonWidget파일안에 있는 함수
+                                          onTap: () async {
+                                            ///이미 내찜목록에 이 컨텐츠가 존재할때 처리 - 파베에서 삭제 로직 진행 + store에서도 삭제
+                                            if (isMyList[index]) {
+                                              ///아이콘 색상 바꾸기위함
+                                              setState(() {
+                                                isMyList[index] = !isMyList[index];
+                                              });
+                                              deleteDoc(index);
+                                            } else {
+                                              ///아이콘 색상 바꾸기위함
+                                              setState(() {
+                                                isMyList[index] = !isMyList[index];
+                                              });
+                                              ///내 찜목록에 이 컨텐츠가 없을때 처리  - 파베에 찜목록 추가로직 진행 + store에도 추가
+                                              addDoc(index);
+                                            }
+                                          }
+                                      ),
+                                    )
+                                  ],
+                                ),
+
+
+                                /// 네이버맵, 태그 /  more버튼
+                                Container(
+                                  margin: EdgeInsets.fromLTRB(10.w, 15.h, 0.w, 0.w),
+                                  child: Row(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Flexible(
+                                        fit: FlexFit.tight,
+                                        flex: 7,
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment
+                                              .spaceAround,
+                                          crossAxisAlignment: CrossAxisAlignment
+                                              .start,
+                                          children: [
+                                            //네이버맵 url scheme값을 이용해서 딥링크 연결하는 동작을 위한 커스텀위젯
+                                            //인자값으로 각 컨텐츠의 풀네임값을 보내줌
+                                            NaverMapDeepLink(
+                                                titlename: grocery_collection[index]['name']),
+
+                                            Container(
+                                              //margin: EdgeInsets.fromLTRB(10.w, 0.h, 7.w, 0.h),
+                                              child: richtext(Icon(
+                                                  Icons.location_on_outlined,
+                                                  color:  Color(0xff706F6F),
+                                                  size: 14.h),
+                                                grocery_collection[index]['address'
+                                                ],),
+                                            ),
+
+                                            Container(
+                                              //margin: EdgeInsets.fromLTRB(10.w, 0.h, 7.w, 0.h),
+                                              child: richtext(Icon(
+                                                  Icons.access_time_outlined,
+                                                  color:  Color(0xff706F6F),
+                                                  size: 14.h),
+                                                  grocery_collection[index]['time']),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+
+                                      ///view more버튼
+                                      Flexible(
+                                          fit: FlexFit.tight,
+                                          flex: 3,
+                                          child: TextButton(onPressed: () { //식당정보 더 보기 버튼
+
+                                            //페이지 이동
+                                            Navigator.push(
+                                                context, MaterialPageRoute(
+                                                builder: (context) =>
+                                                    GroceryShop_hero_second(
+                                                        grocery_collection[index])));
+                                          }, child: moreButton(), ///CommonWidget파일안에 있는 view more버튼
+                                          )
+                                      ),
                                     ],
                                   ),
                                 ),
-                                Container(
-                                  margin: EdgeInsets.fromLTRB(0.w, 3.h, 3.h, 3.h),
-                                  width: 110.w,
-                                  height: 110.w, //이미지
-                                  child: Hero(
-                                      tag: grocery_collection[index]['name'],
-                                      child: Image.network(
-                                        grocery_collection[index]['imagepath'][0],
-                                        fit: BoxFit.cover,)),
-                                )
+
+                                ///문서하나의 이미지들 수평리스트로 띄워주는 함수 - CommonWidget파일안에
+                                getImageList(grocery_collection[index]),
                               ],
-                            ),
-                          )
-                      ),
-                      onTap: () {
-                        grocery_document = grocery_collection[index];
-                        Navigator.push(context, MaterialPageRoute(
-                            builder: (context) =>
-                                GroceryShop_hero_second(grocery_document)));
-                      },
-                    ),
+                            )
+                        ),
                 childCount: count),
-          )
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 40.h,
+            ),
+          ),
         ],
       ),
 
